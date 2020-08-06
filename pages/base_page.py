962eae3096c48c7563d6eb2cb825eaaf830f9490
natalie344 <natalie344@yandex.ru>
2020-08-07 01:48:49 +0300

    added test for login_link and exception

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,15 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, time_out=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(time_out)
     def open(self):
         self.browser.get(self.url)
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except(NoSuchElementException):
+            return False
+        return True
