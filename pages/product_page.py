b49807ed3d17afcc88900e25de8dd289a758ef13
natalie344 <natalie344@yandex.ru>
2020-08-11 01:29:57 +0300

    add test for product page and add to basket

---
+++ pages/product_page.py
@@ -0,0 +1,33 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+from .locators import MainPageLocators
+import time
+
+
+class ProductPage(BasePage):
+    def should_be_product_page(self):
+        self.should_be_write_review()
+        self.should_be_add_to_basket_form()
+
+    def should_be_write_review(self):
+        # проверкa на кнопку написание рецензии
+        assert self.is_element_present(*ProductPageLocators.REVIEW), "Review is not presented"
+
+    def should_be_add_to_basket_form(self):
+        # проверкa, что есть форма логина
+        assert self.is_element_present(*ProductPageLocators.ADD_FORM), "Add to basket form is not presented"
+    
+    def add_to_basket(self):
+        price = self.browser.find_element(*ProductPageLocators.PRICE).text
+        name = self.browser.find_element(*ProductPageLocators.NAME).text
+        button = self.browser.find_element(*ProductPageLocators.ADD_FORM)
+        button.click()
+        self.solve_quiz_and_get_code()
+        messages_book = self.browser.find_element(*ProductPageLocators.MESSAGE_BOOK).text
+        messages_price = self.browser.find_element(*ProductPageLocators.MESSAGE_PRICE).text
+        assert name == messages_book, "Book's title is wrong"
+        assert price == messages_price, "Price is wrong"
+        # time.sleep(10)
+
+
