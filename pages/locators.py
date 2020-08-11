e5939f80c8f71794afd84c8c4788b62808120319
natalie344 <natalie344@yandex.ru>
2020-08-12 01:26:16 +0300

    add tests fo messages and login page from product page

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,22 @@
 from selenium.webdriver.common.by import By
 
-
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_FORM = (By.CSS_SELECTOR, "#add_to_basket_form")
+    REVIEW = (By.CSS_SELECTOR, "#write_review")
+    NAME = (By.CSS_SELECTOR,"#content_inner h1")
+    PRICE = (By.CSS_SELECTOR, "#content_inner p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div.alert-success")
+    MESSAGE_BOOK = (By.CSS_SELECTOR, "#messages div.alert-success div.alertinner strong")
+    MESSAGE_PRICE = (By.CSS_SELECTOR, "#messages div.alert-info div.alertinner strong")
+
--- pages/product_page.py
+++ pages/product_page.py
@@ -29,5 +29,12 @@ class ProductPage(BasePage):
         assert name == messages_book, "Book's title is wrong"
         assert price == messages_price, "Price is wrong"
         # time.sleep(10)
-
-
+    
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+    
+    def should_be_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should be disappeared"
+    
